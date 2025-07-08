5131aa9d0e9103ad6dc81387d947f3b1a15f25d4
elyas-syed <isyedwork@outlook.com>
2025-07-08 02:15:56 -0500

    feat(terraform): add helm release and k8 provider

--- terraform/modules/kind/providers.tf
+++ terraform/modules/kind/providers.tf
@@ -6,5 +6,23 @@ terraform {
       source  = "hashicorp/null"
       version = ">= 3.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.7.1"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.21.1"
+    }
   }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "kind-${var.cluster_name}"
+}
+
+provider "helm" {
 }
