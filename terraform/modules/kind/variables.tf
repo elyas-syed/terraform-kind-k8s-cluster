8cd67ff485b077d99c64e9b17b3a76869da4ec43
elyas-syed <isyedwork@outlook.com>
2025-07-08 21:11:29 -0500

    chore: removing unused variable

--- terraform/modules/kind/variables.tf
+++ terraform/modules/kind/variables.tf
@@ -2,8 +2,3 @@ variable "cluster_name" {
   description = "Name of the Kind cluster"
   type        = string
 }
-
-variable "config_path" {
-  description = "Path to the Kind cluster configuration file"
-  type        = string
-}
